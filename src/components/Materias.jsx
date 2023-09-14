cac23e4f07580e4fa5fd612cd81aaad9704de0c4
Santiago <or.santiago@javeriana.edu.co>
2023-09-14 13:01:38 -0500

    Materia v1

--- src/components/Materias.jsx
+++ src/components/Materias.jsx
@@ -1,7 +1,21 @@
-import {Button} from "@nextui-org/react";
+import {AnimatePresence, motion} from "framer-motion";
 
 export default function Materias() {
     return (
-        <Button>Patico</Button>
+        <AnimatePresence>
+            <motion.div
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ease: "easeOut", duration: 0.5 }}
+            >
+                <div className="flex justify-center items-center m-10">
+                    <h1 className="text-3xl mr-10">Materias</h1>
+                </div>
+                <div className= "flex bg-slate-500 ml-5">
+                        <p className = "text-inherit pl-10">Fundamentos de ingeniería de SW</p>
+                </div>
+
+            </motion.div>
+        </AnimatePresence>
     )
 }
