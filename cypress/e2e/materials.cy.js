5d6da7c2fa16e22c93a9429c3f77c699324d07d4
TalkySafe143 <sebastian.galindo143@gmail.com>
2023-10-19 01:37:02 -0500

    Añadiendo ejemplo de pruebas

--- cypress/e2e/materials.cy.js
+++ cypress/e2e/materials.cy.js
@@ -1,10 +1,17 @@
 describe('template spec', () => {
-  it('Obtiene los materiales existentes', () => {
-    cy.visit('http://localhost:5174')
+  it('Si el estudiante no esta autorizado, no puede acceder a los materiales', () => {
+    cy.visit('http://localhost:5173/materias');
 
-    cy.request('${import.meta.env.VITE_API_URL}/api/material')
+    cy.url().should(url => {
+        expect(url).to.eq('http://localhost:5173/login');
+    })
+
+    cy.request({
+        url: `https://javeplatformapi.2.us-1.fl0.io/api/material`,
+        failOnStatusCode: false
+    })
         .should((response) => {
-            expect(response.status).to.eq(200)
+            expect(response.status).to.eq(401)
         })
 
   })
--- src/components/UserContextWrapper.jsx
+++ src/components/UserContextWrapper.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from 'react';
 
-export const AppContext = React.createContext(null);
+export const AppContext = React.createContext({});
 
 export const UserContextWrapper = (props) => {
     const [ user, setUser ] = useState("");
