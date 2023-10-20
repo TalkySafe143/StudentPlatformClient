1ae4fa17bd99155e348e73d94d5b13b964abe1f5
Santiago <or.santiago@javeriana.edu.co>
2023-10-20 00:35:55 -0500

    Pruebas de tajada Registrar Estudiante

--- cypress/e2e/register.cy.js
+++ cypress/e2e/register.cy.js
@@ -1,35 +1,45 @@
 describe('template spec', () => {
-    beforeEach(() => {
-        cy.visit('http://localhost:5173/register');
-    })
+        it('Registro correctamente', () => {
+            cy.visit('http://localhost:5173/register');
+            cy.get("#CC").type("123");
+            cy.get("#Nombre").type("Santiago");
+            cy.get("#Semestre").type("4");
+            cy.get("#Carrera").type("Ingenieria de Sistemas");
+            cy.get("#C").type("root");
+
+            cy.request({
+                method: 'POST',
+                url: 'https://javeplatformapi.2.us-1.fl0.io/api/auth/login',
+                headers: {
+                    "Authorization" : `Basic ${btoa("123:root")}`
+                },
+                failOnStatusCode: false
+            })
+                .then(response => {
+                    localStorage.setItem("jwt", response.body.token);
+                    localStorage.setItem("user", "123");
+                })
+        })
+
+        it('Registro No válido', () => {
+            cy.visit('http://localhost:5173/register');
+            cy.get("#CC").type("987");
+            cy.get("#Nombre").type("Hernando");
+            cy.get("#Semestre").type("cuatro");
+            cy.get("#Carrera").type("Derecho");
+            cy.get("#C").type("password");
+            cy.request({
+                method: 'POST',
+                url: 'https://javeplatformapi.2.us-1.fl0.io/api/auth/login',
+                headers: {
+                    "Authorization" : `Basic ${btoa("987:password")}`
+                },
+                failOnStatusCode: false
+            })
+                .should(response => {
+                    expect(response.status).to.eq(401)
+                })
+        })
 
 
-    it('Solicita cedula del estudiante', () => {
-      cy.get('#CC').type('123456789{enter}');
-      cy.get('.error-messages')
-        .should('contain', 'Cedula no puede estar vacia')
-    })
-
-    // it('Solicita el nombre del estudiante', () => {
-    //     cy.get('#name').type('Juan');
-    // })
-
-    // it('Solicita el semestre del estudiante', () => {
-    //     cy.get('#semester').type('8');
-    // })
-
-    // it('Solicita la carrera del estudiante', () => {
-    //     cy.get('#career').type('Ingenieria en Sistemas');
-    // })
-
-
-    // it('Si llena todos los campos, se registra correctamente', () => {
-    //     cy.visit('http://localhost:5173/register');
-    //     cy.get('name').type('Juan');
-    //     cy.get('#semester').type('8');
-    //     cy.get('#career').type('Ingenieria en Sistemas');
-    //     cy.get('#password').type('123456');
-
-
-    // })
-})
+});
