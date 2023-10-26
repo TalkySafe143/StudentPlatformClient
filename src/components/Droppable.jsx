2031a429b4785e6a1eb53e898b603827197b5142
Santiago <or.santiago@javeriana.edu.co>
2023-10-26 18:19:07 -0500

    Bug en Droppable

--- src/components/Droppable.jsx
+++ src/components/Droppable.jsx
@@ -1,1 +1,26 @@
-c
+import PropTypes from 'prop-types'; // Importa PropTypes
+import React from 'react';
+import {useDroppable} from '@dnd-kit/core';
+
+export function Droppable(props) {
+    const {isOver, setNodeRef} = useDroppable({
+        id: props.id,
+    });
+    const style = {
+        opacity: isOver ? 1 : 0.5,
+    };
+
+    return (
+        <div ref={setNodeRef} style={style}>
+            {props.children}
+        </div>
+    );
+}
+
+
+// Define prop validations
+Droppable.propTypes = {
+    id: PropTypes.string.isRequired,
+    children: PropTypes.node,
+};
+
