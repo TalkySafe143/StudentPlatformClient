a9e94a3bc06c4682cf16f7f5397f1774f2fb5557
TalkySafe143 <sebastian.galindo143@gmail.com>
2023-10-26 23:42:18 -0500

    Arreglando bug en test sinMaterias

--- cypress/e2e/sinMaterias.cy.js
+++ cypress/e2e/sinMaterias.cy.js
@@ -12,6 +12,7 @@ describe('No existen materias en el sistema', () => {
                 cy.viewport(1920, 1080)
                 localStorage.setItem("jwt", json.body.token);
                 localStorage.setItem("user", "123");
+                localStorage.setItem("test", "Y");
                 cy.visit("http://localhost:5173/materias")
             })
     });
@@ -19,11 +20,51 @@ describe('No existen materias en el sistema', () => {
     after(() => {
         localStorage.removeItem("jwt");
         localStorage.removeItem("user");
+        localStorage.removeItem("test");
     })
 
     it('Cuando se va a agregar alguna materia al estudiante, no hay materias', () => {
 
-       cy.get("#chipMateria").then(element => {
+        cy.request({
+            method: 'GET',
+            url: 'https://javeplatformapi.2.us-1.fl0.io/api/materias',
+            headers: {
+                "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+            },
+            failOnStatusCode: false
+        }).then(res => {
+            const response = res.body.data;
+            response.forEach(materia => {
+                cy.request({
+                    method: 'DELETE',
+                    url: 'https://javeplatformapi.2.us-1.fl0.io/api/materias/'+materia.materia_id,
+                    headers: {
+                        "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+                    },
+                    failOnStatusCode: false
+                })
+            })
+
+            cy.reload();
+
+            cy.get("#chipMateria").should("exist");
+
+            // Reponer las materias
+            response.forEach(materia => {
+                cy.request({
+                    method: 'POST',
+                    url: 'https://javeplatformapi.2.us-1.fl0.io/api/materias/',
+                    headers: {
+                        "Authorization": `Bearer ${localStorage.getItem("jwt")}`
+                    },
+                    failOnStatusCode: false,
+                    body: materia
+                })
+            })
+
+            cy.reload();
+        })
+       /*cy.get("#chipMateria").then(element => {
             let idMateria = element[0].children[0].innerHTML;
             cy.request({
                 url: 'https://javeplatformapi.2.us-1.fl0.io/api/materias/'+idMateria,
@@ -34,7 +75,7 @@ describe('No existen materias en el sistema', () => {
             }).then(res => {
                 expect(res.body.data.length).to.eq(0, "No existen materias")
             })
-        })
+        })*/
 
 
     })
--- src/components/Materias.jsx
+++ src/components/Materias.jsx
@@ -5,26 +5,45 @@ import {Droppable} from './Droppable';
 import {AnimatePresence, motion} from "framer-motion";
 import Navegacion from "./Navegacion.jsx";
 import {useNavigate} from "react-router-dom";
+import {Chip} from "@nextui-org/react";
 
 export default function Materias() {
 
     const navigate =useNavigate();
-
+    const [droppedElements, setDroppedElements] = useState([]);
+    const [parent, setParent] = useState(null);
+    const [draggables, setDragables] = useState([]);
     useEffect(() => {
         if (!localStorage.getItem('jwt')) {
             navigate("/login")
         }
-    }, []);
 
-    const [droppedElements, setDroppedElements] = useState([]);
-    const [parent, setParent] = useState(null);
-    const draggables = [
+        fetch(`${import.meta.env.VITE_API_URL}/api/materias`, {
+            headers: {
+                'Authorization': 'Bearer ' + localStorage.getItem('jwt')
+            }
+        }).then(res => res.json())
+            .then(json => {
+                const drag = [];
+
+                json.data.forEach(materia => {
+                    drag.push(<>
+                        <Draggable key={materia.materia_id} id={materia.materia_id}>{materia.name}</Draggable>
+                    </>)
+                })
+                if (drag.length === 0) drag.push(<><Chip id={"chipMateria"} color={"primary"}>
+                    No existen materias en el sistema, lo sentimos.
+                </Chip></>)
+                setDragables(drag);
+            })
+    }, []);
+    /*const draggables = [
         <Draggable key="draggable1" id="draggable1">Materia 1</Draggable>,
         <Draggable key="draggable2" id="draggable2">Materia 2</Draggable>,
         <Draggable key="draggable3" id="draggable3">Materia 3</Draggable>,
         <Draggable key="draggable4" id="draggable4">Materia 4</Draggable>,
         <Draggable key="draggable5" id="draggable5">Materia 5</Draggable>
-    ];
+    ];*/
 
     return (
         <>
